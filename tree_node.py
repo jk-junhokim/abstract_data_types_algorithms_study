08ced46bbb189cbbe0e435b822dda9b319aca7d7
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2022-08-19 13:54:53 +0000

    completed TreeNode class

--- tree_node.py
+++ tree_node.py
@@ -8,4 +8,38 @@ class TreeNode:
         self.right_child = right
         self.parent = parent
 
+    def has_left_child(self):
+        return self.left_child
+
+    def has_right_child(self):
+        return self.right_child
+
+    def is_left_child(self): # checks if 'self' has parent node and 'self' belongs to parent node
+        return self.parent and self.parent.left_child == self
+
+    def is_right_child(self):
+        return self.parent and self.parent.right_child == self
+
+    def is_root(self):
+        return not self.parent
+
+    def is_leaf(self):
+        return not (self.right_child or self.left_child)
+
+    def has_any_children(self): # boolean value
+        return self.right_child or self.left_child
+
+    def has_both_children(self): # boolean value
+        return self.right_child and self.left_child
+
+    def replace_node_data(self, key, value, lc, rc):
+        self.key = key
+        self.payload = value
+        self.left_child = lc
+        self.right_child = rc
+        if self.has_left_child():
+            self.left_child.parent = self
+        if self.has_right_child():
+            self.right_child.parent = self
+
 
