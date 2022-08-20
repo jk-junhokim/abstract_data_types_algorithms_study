bd110ecb66ed732e8d6d991272f769ce1a18955d
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2022-08-20 13:48:33 +0000

    implemented get method for binary search tree

--- tree_node.py
+++ tree_node.py
@@ -42,4 +42,59 @@ class TreeNode:
         if self.has_right_child():
             self.right_child.parent = self
 
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+        self.size = 0
+
+    def length(self):
+        return self.size
+
+    def __len__(self):
+        return self.size
+
+    def put(self, key, val):
+        if self.root:
+            self._put(key, val, self.root)
+        else:
+            self.root = TreeNode(key, val)
+        self.size = self.size + 1
+
+    def _put(self, key, val, current_node):
+        if key < current_node.key:
+            if current_node.has_left_child():
+                self._put(key, val, current_node.left_child)
+            else:
+                current_node.left_child = TreeNode(key, val, parent = current_node)
+        else:
+            if current_node.has_right_child():
+                self._put(key, val, current_node.right_child())
+            else:
+                current_node.right_child = TreeNode(key, val, parent = current_node)
+
+    def __setitem__(self, k, v):
+        self.put(k, v)
+
+    def get(self, key):
+        if self.root: # a tree exists if there is a root node
+            res = self._get(key, self.root) # start from the root node (top of the tree)
+            if res:
+                return res.payload
+            else:
+                return None
+        else:
+            return None
+
+    def _get(self, key, current_node): # root node == current_node. we want the node, not payload
+        if not current_node: # root node doesnt
+            return None
+        elif current_node.key == key:
+            return current_node
+        elif key < current_node.key:
+            return self._get(key, current_node.left_child)
+        else:
+            return self._get(key, current_node.right_child)
+
+    def __getitem__(self, key):
+        return self.get(key)
 
