baf315f25fca052628fa70e94b6c0c965caabc7f
Junho Kim <89207813+jk-junhokim@users.noreply.github.com>
2022-08-20 14:44:36 +0000

    implemented node removal for node with one child

--- tree_node.py
+++ tree_node.py
@@ -140,6 +140,44 @@ class BinarySearchTree:
                 else:
                     self.parent.right_child = self.right_child
                 self.right_child.parent = self.parent
+
+    def remove(self, current_node):
+        if current_node.is_leaf(): # node has no children
+            if current_node == current_node.parent.left_child:
+                current_node.parent.left_child = None
+            else:
+                current_node.parent.right_child = None
+        elif current_node.has_both_children(): # node both children
+            succ = current_node.find_successor()
+            succ.splice_out()
+            current_node.key = succ.key
+            current_node.payload = succ.payload
+        else: # node has one child
+            if current_node.has_left_child():
+                if current_node.is_left_child():
+                    current_node.left_child.parent = current_node.parent
+                    current_node.parent.left_child = current_node.left_child
+                elif current_node.is_right_child():
+                    current_node.left_child.parent = current_node.parent
+                    current_node.parent.right_child = current_node.left_child
+                else:
+                    current_node.replace_node_data(current_node.left_child.key,
+                                                  current_node.left_child.payload,
+                                                  current_node.left_child.left_child,
+                                                  current_node.left_child.right_child)
+            else:
+                if current_node.is_left_child():
+                    current_node.right_child.parent = current_node.parent
+                    current_node.parent.left_child = current_node.right_child
+                elif current_node.is_right_child():
+                    current_node.right_child.parent = current_node.parent
+                    current_node.parent.right_child = current_node.right_child
+                else:
+                    current_node.replace_node_data(current_node.right_child.key,
+                                                  current_node.right_child.payload,
+                                                  current_node.right_child.left_child,
+                                                  current_node.right_child.right_child)
+                
                 
             
             
